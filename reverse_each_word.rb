e8f50e5662cd1500f966f705c5a136eb88007a07
Emily Jirles <emily.jirles@gmail.com>
2018-04-27 18:41:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split
   result_array = []
-  sentence_array.each{|char| result << char.reverse}
+  sentence_array.each{|char| result_array << char.reverse}
   result_array.join
 end
